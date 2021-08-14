8e6ccd149b2d82c1ec57861d3a3bba50600d16fd
agoalofalife <agoalofalife@gmail.com>
2021-08-14 20:00:22 +0300

    lesson #4

--- app/Http/Livewire/Quiz/Question.php
+++ app/Http/Livewire/Quiz/Question.php
@@ -10,13 +10,51 @@ class Question extends Component
     public string $question;
     public array $options;
     public $answer;
+    public $keyCurrentQuestion;
+    public $questions;
+    public $result;
 
     public function mount(QuestionsRepository $questionsRepository)
     {
-        $questions = $questionsRepository->all();
-        $this->question = $questions[0]['question'];
-        $this->options = $questions[0]['options'];
+        $this->questions = $questionsRepository->all();
+        $this->toggleQuestion();
     }
+
+    public function toggleQuestion()
+    {
+        $currentQuestion = null;
+        foreach ($this->questions as $key => $question) {
+            if (!array_key_exists('passed', $question)) {
+                $currentQuestion = $question;
+                $this->keyCurrentQuestion = $key;
+                $this->question = $currentQuestion['question'];
+                $this->options = $currentQuestion['options'];
+                break;
+            }
+        }
+        if (is_null($currentQuestion)) {
+            $this->result = $this->calculateRightAnswers($this->questions);
+        }
+    }
+
+    public function calculateRightAnswers(array $questions)
+    {
+        return array_reduce($questions, function ($carry, $question) {
+            if ($question['answer_got'] == $question['right_answer']) {
+                $carry = $carry + 1;
+            }
+            return $carry;
+        }, 0);
+    }
+    public function next()
+    {
+        $this->questions[$this->keyCurrentQuestion] = array_merge($this->questions[$this->keyCurrentQuestion], [
+           'passed' => true, 'answer_got' => $this->answer
+        ]);
+        $this->toggleQuestion();
+        $this->reset('answer');
+    }
+
     public function render()
     {
         return view('livewire.quiz.question')->extends('layouts.app');
--- resources/views/livewire/quiz/question.blade.php
+++ resources/views/livewire/quiz/question.blade.php
@@ -1,6 +1,7 @@
 <div class="container container-quiz mt-sm-5 my-1">
     <div class="question ml-sm-5 pl-sm-5 pt-2">
-        <form>
+        @if(is_null($result))
+        <form wire:submit.prevent="next">
             <div class="py-2 h5"><b>{{$question}}</b></div>
             <div class="ml-md-3 ml-sm-3 pl-md-5 pt-sm-0 pt-3" id="options">
                 @foreach($options as $key => $option)
@@ -18,5 +19,10 @@
                 </div>
             </div>
         </form>
+        @else
+            <div class="py-2 h5"><b>Вы прошли тест</b></div>
+            <div class="py-2 h5"><b>Всего правильных ответов {{$result}}</b></div>
+            <div class="py-2 h5"><b>Всего вопросов {{count($questions)}}</b></div>
+        @endif
     </div>
 </div>
